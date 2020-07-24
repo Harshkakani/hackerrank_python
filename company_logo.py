20582fd01e7c76f108d9a9eff84f5a3595e9dfa9
Harsh <harshkakani.hk@gmail.com>
2020-07-24 10:49:37 +0530

    company_logo solution complexity reduced

--- company_logo.py
+++ company_logo.py
@@ -1,24 +1,11 @@
-a = input()
-
-arr = []
-for letter in a:
-    count = 0
-    for i in range(len(a)):
-        if a[i] == letter:
-            count += 1
-    arr.append((letter, count))
-
-arr = list(set(arr))
-arr.sort(key=lambda x: x[1])
-arr = arr[::-1]
-for i in range(len(arr)-1):
-    if arr[i][1] == arr[i+1][1]:
-        if ord(arr[i][0]) > ord(arr[i+1][0]):
-            arr[i],arr[i+1] = arr[i+1],arr[i]
-        else:
-            pass
-    else:
-        pass
-
-for (a,b) in arr[0:3]:
-    print(str(a)+' '+str(b))
+s = input()
+letters = [0]*26
+for letter in s:
+     letters[ord(letter) - ord('a')] += 1
+for i in range(3):
+    max_letter = max(letters)
+    for index in range(26):
+        if max_letter == letters[index]:
+            print(chr(ord('a') + index), max_letter)
+            letters[index] = -1 
+            break
